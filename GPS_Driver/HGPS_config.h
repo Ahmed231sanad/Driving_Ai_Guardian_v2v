97e4174a7effa0a07d8c3bb425b0df48524ca22e
Ahmed Sanad <142271722+Ahmed231sanad@users.noreply.github.com>
2023-12-19 03:15:03 +0200

    Add files via upload

---
+++ GPS_Driver/HGPS_config.h
@@ -0,0 +1,9 @@
+
+#ifndef HGPS_CONFIG_H_
+#define HGPS_CONFIG_H_
+
+
+
+
+
+#endif /* HGPS_CONFIG_H_ */
---
+++ GPS_Driver/HGPS_interface.h
@@ -0,0 +1,18 @@
+
+#ifndef HGPS_INTERFACE_H_
+#define HGPS_INTERFACE_H_
+typedef enum{
+    Done=0,
+    Error=1
+}enumcheckerror;
+void GPS_init(void);
+void GPS_main(void);
+enumcheckerror GPS_enumGetLatLong(u8 * Lat , u8 * Long); // lat must be 11 :ddmm.mmmmm, long must be 12:dddmm.mmmmm
+
+enumcheckerror GPS_enumGetTime(u8 * time); //it must be 9 , hhmmss.ss
+
+enumcheckerror GPS_enumGetSpeed(u8 speed);
+
+enumcheckerror GPS_enumGetDate(u8 * day , u8 * month , u8 * year );
+
+#endif /* GPS_INTERFACE_H_ */
